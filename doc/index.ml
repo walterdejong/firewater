50bffc5baeb24616d676523c1f8cc3420c4dbb4f
Walter de Jong <walter@heiho.net>
2011-11-07 12:28:41 +0100

    start of documentation

---
+++ doc/index.ml
@@ -0,0 +1,20 @@
+<ol>
+ <li><a href="#whatis">What is firewater?</a></li>
+ <li><a href="#install">Installing firewater</a></li>
+ <li><a href="#using">Using firewater</a></li>
+ <li><a href="#syntax">Input file syntax</a></li>
+ <ol>
+  <li><a href="#first">First an example</a></li>
+  <li><a href="#iface">Interfaces</a></li>
+  <li><a href="#network">Networks, ranges, hosts</a></li>
+  <li><a href="#group">Logical groups</a></li>
+  <li><a href="#service">Services</a></li>
+  <li><a href="#policy">Default policy</a></li>
+  <li><a href="#rule">Allow or deny</a></li>
+  <li><a href="#ifdef">Conditional statements: Using ifdefs</a></li>
+  <li><a href="#echo">Injecting native firewall commands</a></li>
+  <li><a href="#include">Standard includes</a></li>
+  <li><a href="#list">List of all keywords</a></li>
+ </ol>
+</ol>
+
